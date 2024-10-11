45845da287becc19db92441a2335a698292d2111
Junaid Irfan <junaid.irfan.2000@gmail.com>
2024-10-10 21:26:53 -0400

    Config changes

--- next.config.mjs
+++ next.config.mjs
@@ -3,9 +3,7 @@ const nextConfig = {
   output: 'export',
   images: {
     unoptimized: true,
-    domains: ['techvify-software.com', 'cdn.britannica.com', 'www.verizon.com'],
   },
-  // Add this line
   basePath: process.env.NODE_ENV === 'production' ? '/junaid.github.io' : '',
 };
 
