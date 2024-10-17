f8fbb26cc40807b70222741963e3b5aa53ad1ec5
Junaid <47072968+junaydirfan@users.noreply.github.com>
2024-10-17 19:21:20 -0400

    Deployment settings

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  assetPrefix: '/',
   images: {
     unoptimized: true,
+  },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.csv$/,
+      use: 'raw-loader',
+    });
+    return config;
   },
 };
 
